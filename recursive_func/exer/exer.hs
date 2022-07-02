cf0003d579aa8d424c8023366258b5ba047cf4cd
ice-kylin <wminid@yeah.net>
2022-07-02 23:47:15 +0100

    update

--- recursive_func/exer/exer.hs
+++ recursive_func/exer/exer.hs
@@ -10,7 +10,7 @@ and' (x : xs) = x && and' xs
 -- 2. concat
 concat' :: [[a]] -> [a]
 concat' [] = []
-concat' (x : xs) = x ++ concat' xs
+concat' (xs : xss) = xs ++ concat' xss
 
 -- 3. replicate
 replicate' :: Int -> a -> [a]
@@ -21,9 +21,9 @@ replicate' n x = x : replicate' (n - 1) x
 infixl 9 <!!>
 
 (<!!>) :: [p] -> Int -> p
-(<!!>) [] _ = error "empty list"
-(<!!>) (x : _) 0 = x
-(<!!>) (_ : xs) n = (<!!>) xs (n - 1)
+[] <!!> _ = error "empty list"
+(x : _) <!!> 0 = x
+(_ : xs) <!!> n = xs <!!> (n - 1)
 
 -- 5. elem
 elem' :: Eq t => t -> [t] -> Bool
@@ -31,5 +31,28 @@ elem' _ [] = False
 elem' e (x : xs) = e == x || elem' e xs
 
 -- exer 2
+merge' :: Ord a => [a] -> [a] -> [a]
+merge' xs [] = xs
+merge' [] xs = xs
+merge' (x : xs) (y : ys)
+  | x > y = y : merge' (x : xs) ys
+  | otherwise = x : merge' xs (y : ys)
 
 -- exer 3
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge' (msort (take half_len xs)) (msort (drop half_len xs))
+  where
+    half_len = length xs `div` 2
+
+-- Insertion Sort
+insert :: Ord a => a -> [a] -> [a]
+insert e [] = [e]
+insert e (x : xs)
+  | e > x = x : insert e xs
+  | otherwise = e : (x : xs)
+
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x : xs) = insert x (isort xs)
